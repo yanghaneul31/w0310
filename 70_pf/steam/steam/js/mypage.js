384a6f3accea8d7eabe6e6b99a47d24b158bc20d
yanghaneul31 <yanghaneul31@gmail.com>
2025-07-15 13:27:33 +0900

    0715

--- 70_pf/steam/steam/js/mypage.js
+++
@@ -1,9 +0,0 @@
-const userName = document.querySelector(".username");
-const editBtn = document.querySelector(".edit-btn");
-const user = localStorage.getItem("loginUser");
-
-userName.textContent = user;
-
-editBtn.addEventListener("click", () => {
-  window.location.href = "./profile_edit.html";
-});
