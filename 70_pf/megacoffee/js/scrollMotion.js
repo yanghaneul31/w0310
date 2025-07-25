607bdbdbbe9de05bdcecb67ec0bb2ac653e19038
yanghaneul31 <yanghaneul31@gmail.com>
2025-07-25 12:05:08 +0900

    0725

--- 70_pf/megacoffee/js/scrollMotion.js
+++ 70_pf/megacoffee/js/scrollMotion.js
@@ -8,9 +8,9 @@ $(() => {
     // 이벤트
     windowObj.scroll(function () {
         motionObj.each(function () {
-            // boxTop은 요소의 문서 기준(top) 위치 (px)
+           
             let boxTop = $(this).offset().top;
-            // scrollTop은 현재 문서에서 위쪽으로 스크롤한 거리 (px)
+           
             let scrollTop = $(window).scrollTop();
             // windowHeight는 브라우저 화면의 높이 (viewport 높이)
             let windowHeight = $(window).height();
